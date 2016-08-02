227a9a98dd856bc437546c1365d021094da0092d
Carlos Pacheco <cpacheco@say23.com>
2016-08-02 14:41:30 -0400

    updates to thingers and stuffages

--- properties.php
+++ properties.php
@@ -1,41 +1,15 @@
 <? include("header.php"); ?>
 
+  <section class="expanded row">
+    <div class="cover-image-container">
+      <div class="cover-image">
+      </div>
+    </div>
+  </section>
   <section class="callout callout-main">
     <div class="row column">
       <h2>the name of the property here and stuff nice and long</h2>
       <p class="lead">Portland, Maine, United States</p>
-    </div>
-  </section>
-  <section class="row">
-    <div class="columns">
-      <div class="orbit" role="region" aria-label="Favorite Space Pictures" data-orbit>
-        <ul class="orbit-container">
-          <button class="orbit-previous"><span class="show-for-sr">Previous Slide</span>&#9664;&#xFE0E;</button>
-          <button class="orbit-next"><span class="show-for-sr">Next Slide</span>&#9654;&#xFE0E;</button>
-          <li class="is-active orbit-slide">
-            <img class="orbit-image" src="static/images/orbit/01.jpg" alt="Space">
-            <figcaption class="orbit-caption">Space, the final frontier.</figcaption>
-          </li>
-          <li class="orbit-slide">
-            <img class="orbit-image" src="static/images/orbit/02.jpg" alt="Space">
-            <figcaption class="orbit-caption">Lets Rocket!</figcaption>
-          </li>
-          <li class="orbit-slide">
-            <img class="orbit-image" src="static/images/orbit/03.jpg" alt="Space">
-            <figcaption class="orbit-caption">Encapsulating</figcaption>
-          </li>
-          <li class="orbit-slide">
-            <img class="orbit-image" src="static/images/orbit/04.jpg" alt="Space">
-            <figcaption class="orbit-caption">Outta This World</figcaption>
-          </li>
-        </ul>
-        <nav class="orbit-bullets">
-          <button class="is-active" data-slide="0"><span class="show-for-sr">First slide details.</span><span class="show-for-sr">Current Slide</span></button>
-          <button data-slide="1"><span class="show-for-sr">Second slide details.</span></button>
-          <button data-slide="2"><span class="show-for-sr">Third slide details.</span></button>
-          <button data-slide="3"><span class="show-for-sr">Fourth slide details.</span></button>
-        </nav>
-      </div>
     </div>
   </section>
   <section class="row" id="content">
